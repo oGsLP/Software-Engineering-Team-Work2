6a8dcba8243e9748b7dacb5b223d242e55ac9271
py-task <2529716798@qq.com>
2017-11-29 18:22:55 +0800

    fix

--- stage4/code/src/database/GetConnection.java
+++ stage4/code/src/database/GetConnection.java
@@ -1,28 +1,50 @@
 package database;
 import java.sql.*;
+
 public class GetConnection {
     public static void main(String[] args){
+        Connection con = null;
+        Statement sta = null;
+        ResultSet res = null;
+        String sql = null;
+        String url = new String("jdbc:mysql://localhost:3306/demo1?characterEncoding=utf8&useSSL=false");
         try{
-            //调用Class.forName()方法加载驱动程序
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("成功加载MySQL驱动！");
-        }catch(ClassNotFoundException e1){
-            System.out.println("找不到MySQL驱动!");
-            e1.printStackTrace();
-        }
 
-        String url="jdbc:mysql://localhost:3306/mysql";    //JDBC的URL
-        //调用DriverManager对象的getConnection()方法，获得一个Connection对象
-        Connection conn;
-        try {
-            conn = DriverManager.getConnection(url,    "root","");
-            //创建一个Statement对象
-            Statement stmt = conn.createStatement(); //创建Statement对象
-            System.out.print("成功连接到数据库！");
-            stmt.close();
-            conn.close();
-        } catch (SQLException e){
-            e.printStackTrace();
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url,"root","1212hjkl");
+            sta = con.createStatement();
+            System.out.println("Connected to database...");
+            sql = new String("select * from student");
+            res = sta.executeQuery(sql);
+            System.out.println("name     sex     age");
+            while(res.next()){
+                System.out.print(res.getString("name"));
+                System.out.print("     ");
+                System.out.print(res.getString("sex"));
+                System.out.print("     ");
+                System.out.print(res.getString("age") + "\n");
+            }
+        }catch (ClassNotFoundException e){
+            System.out.println("ClassNotFoundException");
+
+        }catch (SQLException a){
+            System.out.println("Fail to connect to database!");
+        }
+        catch (Exception b){
+
+        }
+        finally {
+            try{
+                if(con != null)
+                    con.close();
+                if (sta != null)
+                    sta.close();
+                if(res != null)
+                    res.close();
+            }catch (Exception e){
+
+            }
         }
     }
+
 }
