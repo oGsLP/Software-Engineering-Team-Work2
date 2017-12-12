ef0b75a6500c68c605e3db0651b9f53d5b5efaf5
py-task <2529716798@qq.com>
2017-12-12 13:15:03 +0800

    dateHelper

--- src/objects/DateHelper.java
+++ src/objects/DateHelper.java
@@ -1,53 +1,24 @@
 package objects;
 
-import po.SalePO;
-import po.SaleReturnPO;
-import po.StockPO;
-import po.StockReturnPO;
-
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
- * 辅助增加编号的，重载 * 4，
+ * 日期是辅助工具，获取日期，判断日期
  */
 public class DateHelper {
-
-   public void addHelper(SalePO po){
-
-
-   }
-
-   public void addHelper(SaleReturnPO po){}
-
-   public void addHelper(StockPO po){
-      String operation = "from Stock";
-      ArrayList<StockPO> list = (ArrayList<StockPO>)HQLTools.find(operation);
-      int idx = list.size() - 1;
-      if(idx == -1) {
-         HQLTools.add(po);
-         return;
-      }
-      StockPO lastPO = list.get(idx);
-      if(!isSameDay(po.getDate(), lastPO.getDate()))
-         po.setReceiptNumber(1);
-      HQLTools.add(po);
-
-   }
-
-   public void addHelper(StockReturnPO po){
-
-   }
-
-   public static String getDate(){
+   public String getDate(){
       Calendar calendar = Calendar.getInstance();
-      return null;
+      Date date = calendar.getTime();
+      SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+      String s = f.format(date);
+      return s;
    }
 
-   public static boolean isSameDay(String date1, String date2){
-      if(date1.equals(date2))
-         return true;
-      return false;
+   public boolean isSameDay(String date1, String date2){
+      return date1.equals(date2);
    }
+
 
 }
--- src/test/datatest/DateHelperTest.java
+++ src/test/datatest/DateHelperTest.java
@@ -2,46 +2,31 @@ package test.datatest;
 
 import objects.DateHelper;
 import org.junit.Test;
-import po.StockPO;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class DateHelperTest {
     DateHelper dateHelper = new DateHelper();
     @Test
-    public void addHelper() throws Exception {
-    }
-
-    @Test
-    public void addHelper1() throws Exception {
-    }
-
-    @Test
-    public void addHelper2() throws Exception {
-        StockPO po1 = new StockPO("JHD-2017-12-11-00001", "2017-12-11", 1, "py", 100,
-                "xiao", null,"", 100);
-        dateHelper.addHelper(po1);
-
-        assertEquals("JHD-2017-12-11-00001", po1.getNumber());
-
-        StockPO po2 = new StockPO("JHD-2017-12-11-00002", "2017-12-11", 2, "py", 100,
-                "xiao", null,"", 100);
-        dateHelper.addHelper(po2);
-        assertEquals("JHD-2017-12-11-00002", po2.getNumber());
-
-        StockPO po3 = new StockPO("JHD-2017-12-12-00004", "2017-12-12", 4, "py", 100,
-                "xiao", null,"", 100);
-        dateHelper.addHelper(po3);
-        assertEquals("JHD-2017-12-12-00004", po3.getNumber());
-        assertEquals(1, po3.getReceiptNumber());
-    }
-
-    @Test
-    public void addHelper3() throws Exception {
+    public void getDate() throws Exception {
     }
 
     @Test
     public void isSameDay() throws Exception {
+        System.out.println(dateHelper.getDate());
+
+        String s1 = "2017-12-12";
+        String s2 = "2017-12-12";
+        String s3 = "2016-12-12";
+        String s4 = "2017-12-13";
+
+        boolean result1 = dateHelper.isSameDay(s1, s2);
+        boolean result2 = dateHelper.isSameDay(s1, s3);
+        boolean result3 = dateHelper.isSameDay(s1, s4);
+        assertEquals(true, result1);
+        assertEquals(false, result2);
+        assertEquals(false, result3);
+
     }
 
 }
